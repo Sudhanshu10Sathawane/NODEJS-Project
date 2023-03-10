c91a42cc0a1e4164a34b2035d127f9bdd565984b
sudhanshu10sathawane <sudhanshusathwane@gmail.com>
2023-03-10 10:30:58 +0530

    comment added

--- Assignment/app.js
+++ Assignment/app.js
@@ -3,6 +3,7 @@ const app=express();
 app.use('/user',(req,res,next)=>{
     console.log('Hii I am middleware');
 });
+//Route Request and get Response
 app.use('/',(req,res,next)=>{
     console.log('Hii I am another middleware');
     res.send('<h1> Sudha HIII</h1>');
