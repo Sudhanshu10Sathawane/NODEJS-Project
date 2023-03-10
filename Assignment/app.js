0a940b8daa1ae185a118b7b312a8cff88d4b208e
sudhanshu10sathawane <sudhanshusathwane@gmail.com>
2023-03-10 08:38:46 +0530

    All files

---
+++ Assignment/app.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const app=express();
+app.use('/user',(req,res,next)=>{
+    console.log('Hii I am middleware');
+});
+app.use('/',(req,res,next)=>{
+    console.log('Hii I am another middleware');
+    res.send('<h1> Sudha HIII</h1>');
+});
+
+app.listen(3000);
